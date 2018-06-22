2f2e92c8e69d53f6820eef6c8c098848f8a3afb5
Olegzandr Denman <alex@oresoftware.com>
2018-06-21 21:06:31 -0700

    pdev:'set'

--- cli/npmv.sh
+++ cli/npmv.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+export npmv_gray='\033[1;30m'
+export npmv_magenta='\033[1;35m'
+export npmv_cyan='\033[1;36m'
+export npmv_orange='\033[1;33m'
+export npmv_yellow='\033[1;33m'
+export npmv_green='\033[1;32m'
+export npmv_no_color='\033[0m'
+
+zmx(){
+	 "$@"  \
+        2> >( while read line; do echo -e "${r2g_magenta}[${zmx_command_name}]${r2g_no_color} $line"; done ) \
+        1> >( while read line; do echo -e "${r2g_gray}[${zmx_command_name}]${r2g_no_color} $line"; done )
+}
 
 first_arg="$1"
 shift 1;
@@ -9,14 +22,22 @@ mkdir -p "$npmvv"
 
 if [ "$first_arg" == "use" ]; then
 
-    npmv_use "$@"
+    export zmx_command_name="npmv/use"
+    zmx npmv_use "$@"
+
+elif [ "$first_arg" == "ls" ]; then
+
+    export zmx_command_name="npmv/ls"
+    zmx ls "$npmvv"
+    zmx echo "Total number of installations: $(ls "$npmvv" | wc -l)"
 
 elif [ "$first_arg" == "rm" ] || [ "$first_arg" == "remove" ]; then
 
-    npmv_rm "$@"
+    export zmx_command_name="npmv/rm"
+    zmx npmv_rm "$@"
 
 else
 
- echo "no command recognized";
+ echo "no command recognized, try (kk5 rm, kk5 use, kk5 install, kk5 ls)";
 
 fi
--- cli/npmv_rm.sh
+++ cli/npmv_rm.sh
@@ -3,7 +3,8 @@
 set -e;
 desired_npm_version="$1"
 
-mkdir -p "$HOME/.npmv_stash/versions";
+export npmvv="$HOME/.npmv_stash/versions"
+mkdir -p "$npmvv";
 
 #if [ -z "$(which read_json)" ]; then
 #    npm install -g "@oresoftware/read.json" || {
@@ -18,16 +19,25 @@ if [ -z "$desired_npm_version" ]; then
 fi
 
 current_npm_version="$(npm -v)";
+echo "Current npm version: $(npm -v)"
 read_link="$(readlink -f $(which npm))"
+echo "Current npm install location: $read_link";
+echo "Current number of installations: $(ls "$npmvv" | wc -l)"
 
-find "$HOME/.npmv_stash/versions" -type d -name "$desired_npm_version" | while read line; do
+nothing_removed="yes";
 
+while read line; do
+
+ nothing_removed="no";
  npm_install_dir="$line";
 
- if [ "$read_link" == "$npm_install_dir"* ]; then
+
+ echo "Found a matching installation directory: $npm_install_dir";
+
+ if [[ ${read_link} =~ ^$npm_install_dir ]]; then
      echo "Found directory with your current npm version, will attempt to install new npm to replace it."
-     npm install -g "$current_npm_version" || {
-       echo "Could not install new npm version $current_npm_version";
+     npm install -g "npm@$current_npm_version" || {
+       echo -e "${npmv_magenta}Could not install new npm version $current_npm_version${npmv_no_color}";
        exit 1;
      }
  fi
@@ -35,13 +45,16 @@ find "$HOME/.npmv_stash/versions" -type d -name "$desired_npm_version" | while r
  rm -r "$npm_install_dir";
 
   if [ "$?" == "0" ]; then
-
-      echo "Removed installation at directory => $npm_install_dir";
+      echo -e "Removed installation at directory => $npm_install_dir";
   else
-       echo >&2 "Could not remove dir => $npm_install_dir";
+      echo -e >&2 "Could not remove dir => ${npmv_magenta}$npm_install_dir${npmv_no_color}";
   fi
 
 
-done
+done < <(find "$HOME/.npmv_stash/versions" -type d -name "$desired_npm_version")
 
+if [[ "$nothing_removed" == "yes" ]]; then
+   echo -e "${npmv_magenta}Zero npm installations were removed.${npmv_no_color}";
+fi
 
+echo "Number of installations remaining: $(ls "$npmvv" | wc -l)"
